b740a6689c390a9ae5a69033ed8f8d7a4cbd105e
benjamin koh <github@thekohs.net>
2023-09-16 16:06:30 +0800

    profit edit photo button is done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :validatable
   has_many :earned_badges
   has_many :user_game_data
+  has_one_attached :profile_photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 end
