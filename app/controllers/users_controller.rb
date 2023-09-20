732fdea63a2e3bdbd7dfc1815fcc9032ed731a8d
lim yi xuan <limyx1215@hotmail.com>
2023-09-20 22:47:11 +0800

    functional display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @user_xp_earned = 200
+    # @user.user_game_data.sum(:game_xp)
+    @user_level = calculate_level(@user_xp_earned)
+    @xp_left_to_next_level = xp_to_next_level(@user_xp_earned, @user_level)
+    @xp_progress_percentage = xp_progress_percentage(@user_xp_earned, @xp_left_to_next_level)
     @average_workout_time = @user.average_time
     @workout_finished = @user.user_game_data.where(finish: true).count
     @bonus_games_started = @user.user_game_data.where(bonus_finish: [true, false]).count
@@ -11,5 +16,21 @@ class UsersController < ApplicationController
     @multi_games_won = @user.user_game_data.joins(:room).where(room: { mode: 'multi', winner_user_id: @user_id }).count
     @multi_games_lost = @user.user_game_data.joins(:room).where(room: { mode: 'multi' }).where.not(room: { winner_user_id: @user_id }).count
     @multi_games_quit = @user.user_game_data.joins(:room).where(room: { mode: 'multi', winner_user_id: @user_id }, finish: false).count
+  end
+
+  private
+
+  def calculate_level(_user_xp_earned)
+    base_xp_increment = 200
+    (@user_xp_earned / base_xp_increment).to_i + 1
+  end
+
+  def xp_to_next_level(_user_xp_earned, user_level)
+    base_xp_increment = 200
+    user_level * base_xp_increment
+  end
+
+  def xp_progress_percentage(user_xp_earned, xp_left_to_next_level)
+    (user_xp_earned.to_f / xp_left_to_next_level * 100).round(2)
   end
 end
