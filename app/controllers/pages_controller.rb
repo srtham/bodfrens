17ebfe713a5aa2a060d9cebe841935fd020dd93b
lim yi xuan <limyx1215@hotmail.com>
2023-09-23 14:06:05 +0800

    amended the pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,13 @@ class PagesController < ApplicationController
   def home
     if user_signed_in? == false
       redirect_to new_user_session_path
+    else
+      user_id = session[:user_id]
+      @user = User.find(user_id)
+      @workout_finished = @user.user_game_data.where(finish: true).count
+      @user_xp_earned = @user.user_game_data.sum(:game_xp)
+      @user_level = calculate_level(@user_xp_earned)
     end
-
-    user_id = session[:user_id]
-    @user = User.find(user_id)
-    @workout_finished = @user.user_game_data.where(finish: true).count
-    @user_xp_earned = @user.user_game_data.sum(:game_xp)
-    @user_level = calculate_level(@user_xp_earned)
   end
 
   def mode_select
