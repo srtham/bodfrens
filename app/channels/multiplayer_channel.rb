c640ac22fa33dea851770dfa4c8730933746b1c0
benjamin koh <github@thekohs.net>
2023-09-26 09:13:53 +0800

    MP now sents messages

--- app/channels/multiplayer_channel.rb
+++ app/channels/multiplayer_channel.rb
@@ -2,10 +2,15 @@ class MultiplayerChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
     multiplayerroom = Multiplayerroom.find(params[:multiplayerroom_id])
+    stream_form multiplayerroom
     stream_for multiplayerroom
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def relay(data)
+    ActionCable.server.broadcast("multiplayer_channel", message: data["message"])
   end
 end
--- app/javascript/controllers/multiplayerroom_subscription_controller.js
+++ app/javascript/controllers/multiplayerroom_subscription_controller.js
@@ -1,10 +1,34 @@
 import { Controller } from "@hotwired/stimulus"
 import { createConsumer } from "@rails/actioncable"
 
+const consumer = createConsumer(); // Create a consumer instance
+
 // Connects to data-controller="multiplayerroom-subscription"
 export default class extends Controller {
   static values = { multiplayerroomId: Number }
-  static targets = [ "messages" ]
+  static targets = ["button", "bonusButton"]
+
+  markComplete(event) {
+    // Logic for marking as complete
+    // Send message to subscribed channel
+    console.log("Exercise completed!")
+    consumer.subscriptions.create({ channel: "MultiPlayerRoomChannel" }, {
+      received(data) {
+        console.log(data)
+      }
+    })
+  }
+
+  markBonusComplete(event) {
+    // Logic for marking as complete
+    // Send message to subscribed channel
+    console.log("Bonus Exercise completed!")
+    consumer.subscriptions.create({ channel: "MultiPlayerRoomChannel" }, {
+      received(data) {
+        console.log(data)
+      }
+    })
+  }
 
   connect() {
     this.channel = createConsumer().subscriptions.create(
